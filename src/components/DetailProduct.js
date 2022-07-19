cff27c07c6d1c7cc78fc97722e90366ef5eff76f
Jsubak <tnwjd1003@gmail.com>
2022-07-19 19:03:58 +0900

    디테일에 있던 것 주문 구현

--- src/components/DetailProduct.js
+++ src/components/DetailProduct.js
@@ -2,26 +2,12 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import ProductService from "../services/ProductService";
 import { Link } from "react-router-dom";
-import OrderService from "../services/orders.service";
-import { useNavigate } from 'react-router-dom';
-
 
 const DetailProduct = () => {
-    let navigate = useNavigate(); 
 
     const { id } = useParams();
 
     const [currentProduct, setCurrentProduct] = useState("");
-
-    const [orders, setOrders] = useState({})
-
-    useEffect(() => {
-        const getCurrentUser = JSON.parse(localStorage.getItem("user"));
-        if (getCurrentUser) {
-          setOrders({userid: getCurrentUser.userid,
-          productid: id})
-        }
-    }, [id]);
 
     const getProduct = id => {
         ProductService.get(id)
@@ -39,22 +25,6 @@ const DetailProduct = () => {
             getProduct(id);
     }, [id]);
 
-    const saveOrders = () => {
-        var data = {
-            userid: orders.userid,
-            productid: orders.productid,
-        }
-
-        OrderService.create(data)
-            .then(response => {
-                navigate("/orders");
-                console.log(response.data);
-            })
-            .catch(e => {
-                console.log(e)
-            })
-    }
-
     return (
         <div>
             <div className="">
@@ -62,7 +32,7 @@ const DetailProduct = () => {
                 <div>{currentProduct.productname}</div>
                 <div>{currentProduct.productdes}</div>
                 <div><img src={currentProduct.productimg} alt="img2"/></div>
-                <button onClick={saveOrders}>주문하기</button>
+                <Link to="/orders" state={{id: id, name: currentProduct.productname}}>주문하기</Link>
             </div>
         </div>
     );
--- src/components/Order.js
+++ src/components/Order.js
@@ -1,10 +1,90 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect} from "react";
+import { Link, useLocation } from "react-router-dom";
+import OrderService from "../services/orders.service";
+import { Modal, Button } from "antd";
+import DaumPostCode from "react-daum-postcode"
+import 'antd/dist/antd.css';
 
 const Order = () => {
+
+    const getCurrentUser = JSON.parse(localStorage.getItem("user"));
+
+    const location = useLocation();
+    const { id, name } = location.state
+
+    const [orders, setOrders] = useState({
+        userid: getCurrentUser.userid,
+        productid: id,
+    })
+
+    const onChangeValue = e => {
+        const { name, value } = e.target
+        setOrders({...orders, [name]: value})
+    }
+
+    const saveOrders = () => {
+        var data = {
+            userid: orders.userid,
+            productid: orders.productid,
+            address: orders.address,
+            phone: orders.phone
+
+        }
+
+        OrderService.create(data)
+            .then(response => {
+                setOrders({
+                    address: response.data.address,
+                    phone: response.data.phone
+                })
+                console.log(response.data);
+            })
+            .catch(e => {
+                console.log(e)
+            })
+    }
+
+    const [mycode, setMycode] = useState("")
+    const [myaddress, setMyaddress] = useState("");
+    const [isOpen, setIsOpen] = useState(false)
+
+    const handleComplete = (data) => {
+        // let fullAddress = data.address;
+        // let extraAddress = "";
+        // if (data.addressType === "R") {
+        //     if(data.bname !== "") {
+        //         extraAddress += data.bname
+        //     }
+        //     if(data.bulidingName !== "") {
+        //         extraAddress += (extraAddress !== "" ? `, ${data.bulidingName}` : data.bulidingName)
+        //     }
+        //     fullAddress += (extraAddress !== "" ? `(${extraAddress})` : "")
+        // }
+        setMycode(data.zonecode);
+        setMyaddress(data.address)
+        console.log(data.zonecode, data.address)
+        setIsOpen((prev) => !prev)
+    }
+
+    function onToggle() {
+        setIsOpen((prev) => !prev)
+    }
+
+
     return(
         <div>
-            <div>주문완료!</div>
+            <label>주소</label>
+            <input type="text" name="address" value={orders.address || ""} onChange={onChangeValue} />
+            <Button type="primary" onClick={onToggle}>주소 검색</Button>
+            {isOpen && (
+                <Modal title="Basic Modal" visible={true} onCancel={onToggle}>
+                <DaumPostCode onComplete={handleComplete} />
+              </Modal>
+            )}
+            <label>연락처</label>
+            <input type="text" name="phone" value={orders.phone || ""} onChange={onChangeValue} />
+            <div>상품 이름 : {name}</div>
+            <button onClick={saveOrders}>주문 ㄲ</button>
             <Link to={"/product"}>상품목록으로 돌아가기</Link>
         </div>
     )
