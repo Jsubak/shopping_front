f52791a1d50a2082557a168fc2e600df9945b4da
Jsubak <tnwjd1003@gmail.com>
2022-07-25 18:48:59 +0900

    안쓰고 있는 코드 주석처리

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,16 +1,22 @@
-import http from "../http-common";
+// import http from "../http-common";
 
-const create = data => {
-    return http.post("/user", data);
-}
+// const create = data => {
+//     return http.post("/user", data);
+// }
 
-const findById = userid => {
-    return http.get(`/user?userid=${userid}`);
-}
+// const findById = userid => {
+//     return http.get(`/user?userid=${userid}`);
+// }
+
+// const finduser = (params) => {
+//     return http.get( "/user", )
+// }
 
 const UserService = {
-    create,
-    findById
+    // create,
+    // findById,
+    // finduser
+
 }
 
 export default UserService
---
+++ src/services/mypage.service.js
@@ -0,0 +1,11 @@
+// import http from "../http-common";
+
+// const finduser = (params) => {
+//     return http.get( "/mypage", {params})
+// }
+
+// const MypageService = {
+//     finduser
+// }
+
+// export default MypageService
