fa127daaaa66852c5d3e2dab3bfe6f270d862fb5
Jsubak <tnwjd1003@gmail.com>
2022-07-26 18:51:19 +0900

    마이페이지 탭 구현

--- src/components/Mypage.js
+++ src/components/Mypage.js
@@ -1,11 +1,57 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import UserService from "../services/UserService";
-import { Link } from "react-router-dom";
-// import MypageService from "../services/mypage.service"
+import moment from "moment";
+
+import "./css/mypage.css"
+
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+import MypageOne from "./MypageOne";
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+  
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`vertical-tabpanel-${index}`}
+        aria-labelledby={`vertical-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box sx={{ p: 3 }}>
+            {children}
+          </Box>
+        )}
+      </div>
+    );
+  }
+  
+  TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
+  };
+  
+  function a11yProps(index) {
+    return {
+      id: `vertical-tab-${index}`,
+      'aria-controls': `vertical-tabpanel-${index}`,
+    };
+  }
 
 const Mypage = () => {
     const [users, setUser] = useState({})
+    const [value, setValue] = useState(0);
+    const [date2, setDate] = useState();
+
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
     
     const getCurrentUser = JSON.parse(localStorage.getItem("user"));
 
@@ -18,6 +64,8 @@ const Mypage = () => {
         .then(response => {
             setUser(response.data)
             console.log(response.data)
+            const date = moment(response.data).format("YYYY-MM-DD")
+            setDate(date)
         })
         .catch(e => {
             console.log(e)
@@ -26,14 +74,48 @@ const Mypage = () => {
     
     return (
         <div>
-            <div>
-                {users.userid}
+            <div className="mypagebox">
+                <img src={"images/img1.jpg"}  alt="profileimg"/>
+                <Box className="mypagebox-list" sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex', height: 224 }}>
+                <Tabs
+                    orientation="vertical"
+                    variant="scrollable"
+                    value={value}
+                    onChange={handleChange}
+                    aria-label="Vertical tabs example"
+                    sx={{ borderRight: 1, borderColor: 'divider' }}
+                >
+                    <Tab label="내 정보" {...a11yProps(0)} />
+                    <Tab label="주문확인" {...a11yProps(1)} />
+                    <Tab label="내 정보 변경" {...a11yProps(2)} />
+                    <Tab label="찜한 상품" {...a11yProps(3)} />
+                </Tabs>
+                    <TabPanel value={value} index={0}>
+                        <div className="tab-item">
+                            아이디 : {users.userid}
+                        </div>
+                        <div className="tab-item">
+                            이름 : {users.username}
+                        </div>
+                        <div className="tab-item">
+                            이메일 : {users.email}
+                        </div>
+                        <div className="tab-item">
+                            가입 날짜 : {date2}
+                        </div>
+                    </TabPanel>
+                    <TabPanel value={value} index={1}>
+                        <MypageOne/>
+                    </TabPanel>
+                    <TabPanel value={value} index={2}>
+                        Item Three
+                    </TabPanel>
+                    <TabPanel value={value} index={3}>
+                        Item Four
+                    </TabPanel>
+                </Box>
             </div>
-            <div>
-                {users.username}
-            </div>
-                {/* {orders.address} */}
-                <Link to={"/orderinfo"}>이동</Link>
+            {/* <footer></footer> */}
         </div>
     )
 }
--- src/components/MypageOne.js
+++ src/components/MypageOne.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
+import "./css/mypage.css"
+
 const MypageOne = () => {
     const [orders, setOrders] = useState([])
     
@@ -26,9 +28,12 @@ const MypageOne = () => {
             {orders &&
                 orders.map((order, index) => (
                     <div className="list-box-item" key={index}>
-                        {order.productid},{order.productcount}
-                        {order.phone},{order.productprice}
-                        <div className="list-box-price">{order.productprice}</div>
+                        <div className="box-item">상품 번호 : {order.productid}</div>
+                        <div className="box-item">주문수량 : {order.productcount} </div>
+                        <div className="box-item">총금액 : {order.productprice}</div>
+                        <div className="box-item">핸드폰 : {order.phone}</div>
+                        <div className="box-item">주소 : {order.address}</div>
+                        <div className="box-item">주문날짜 : {order.orderdate.slice(0,10)}</div>
                     </div> 
             ))}
         </div>
